d88dec8f358800d441fc82437f660281bbe05857
GollaBhavana7 <161456145+GollaBhavana7@users.noreply.github.com>
2024-11-10 12:39:47 +0530

    Update mdps_public.py

--- mdpd/mdps_public.py
+++ mdpd/mdps_public.py
@@ -141,9 +141,7 @@ if (selected == 'Heart Disease Prediction'):
     # creating a button for Prediction
     
     if st.button('Heart Disease Test Result'):
-        features = [float(age), sex, cp, float(trestbps), float(chol), fbs, restecg, float(thalach), exang, float(oldpeak), slope, ca, thal]
-        heart_prediction = heart_disease_model.predict([features])  # Wrap features in another list to create a 2D array
-        #heart_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])                          
+        heart_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])                          
         
         if (heart_prediction[0] == 1):
           heart_diagnosis = 'The person is having heart disease'
