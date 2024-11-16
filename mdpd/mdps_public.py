62b477aef560d94b3d046c6bc12b635e62bef191
GollaBhavana7 <161456145+GollaBhavana7@users.noreply.github.com>
2024-11-16 17:19:45 +0530

    Update mdps_public.py

--- mdpd/mdps_public.py
+++ mdpd/mdps_public.py
@@ -63,7 +63,7 @@ with st.sidebar:
                 "Home",
                 "Diabetes Prediction",
                 "Heart Disease Prediction",
-                "Parkinsons Prediction",
+                "Parkinson's Prediction",
                 "Logout",
             ],
             icons=["house", "activity", "heart", "person", "box-arrow-right"],
@@ -159,14 +159,10 @@ if st.session_state.logged_in:
                 [[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]]
             )
             result = "The person is diabetic" if diab_prediction[0] == 1 else "The person is not diabetic"
-            st.success(result)
+            st.success(f"Patient: {patient_name}\nAge: {Age}\nResult: {result}")
 
-# Add similar sections for "Heart Disease Prediction" and "Parkinsons Prediction"
- 
 # Heart Disease Prediction Page
-if (selected == 'Heart Disease Prediction'):
-    
-    # page title
+elif selected == "Heart Disease Prediction":
     st.title('Heart Disease Prediction using ML')
     
     col1, col2, col3 = st.columns(3)
@@ -210,27 +206,20 @@ if (selected == 'Heart Disease Prediction'):
     with col1:
         thal = st.number_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
         
-        
-     
-     
-    # code for Prediction
-    heart_diagnosis = ''
-    
-    # creating a button for Prediction
-    
     if st.button('Heart Disease Test Result'):
-        heart_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])                          
+        heart_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
         
-        if (heart_prediction[0] == 1):
-          heart_diagnosis = 'The person is having heart disease'
+        heart_diagnosis = ''
+        
+        if heart_prediction[0] == 1:
+            heart_diagnosis = 'The person is having heart disease'
         else:
-          heart_diagnosis = 'The person does not have any heart disease'
-        
-    st.success(heart_diagnosis)
-        
+            heart_diagnosis = 'The person does not have any heart disease'
+
+        st.success(f"Patient: {age}\nSex: {'Male' if sex == 1 else 'Female'}\nResult: {heart_diagnosis}")
+
 # Parkinson's Prediction Page
-if selected == "Parkinsons Prediction":
-    # page title
+elif selected == "Parkinson's Prediction":
     st.title("Parkinson's Disease Prediction using ML")
 
     col1, col2, col3, col4, col5 = st.columns(5)
@@ -248,76 +237,4 @@ if selected == "Parkinsons Prediction":
         Jitter_percent = st.text_input('MDVP:Jitter(%)')
 
     with col5:
-        Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
-
-    with col1:
-        RAP = st.text_input('MDVP:RAP')
-
-    with col2:
-        PPQ = st.text_input('MDVP:PPQ')
-
-    with col3:
-        DDP = st.text_input('Jitter:DDP')
-
-    with col4:
-        Shimmer = st.text_input('MDVP:Shimmer')
-
-    with col5:
-        Shimmer_dB = st.text_input('MDVP:Shimmer(dB)')
-
-    with col1:
-        APQ3 = st.text_input('Shimmer:APQ3')
-
-    with col2:
-        APQ5 = st.text_input('Shimmer:APQ5')
-
-    with col3:
-        APQ = st.text_input('MDVP:APQ')
-
-    with col4:
-        DDA = st.text_input('Shimmer:DDA')
-
-    with col5:
-        NHR = st.text_input('NHR')
-
-    with col1:
-        HNR = st.text_input('HNR')
-
-    with col2:
-        RPDE = st.text_input('RPDE')
-
-    with col3:
-        DFA = st.text_input('DFA')
-
-    with col4:
-        spread1 = st.text_input('spread1')
-
-    with col5:
-        spread2 = st.text_input('spread2')
-
-    with col1:
-        D2 = st.text_input('D2')
-
-    with col2:
-        PPE = st.text_input('PPE')
-
-    # code for Prediction
-    parkinsons_diagnosis = ''
-
-    # creating a button for Prediction    
-    if st.button("Parkinson's Test Result"):
-
-        user_input = [fo, fhi, flo, Jitter_percent, Jitter_Abs,
-                      RAP, PPQ, DDP,Shimmer, Shimmer_dB, APQ3, APQ5,
-                      APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE]
-
-        user_input = [float(x) for x in user_input]
-
-        parkinsons_prediction = parkinsons_model.predict([user_input])
-
-        if parkinsons_prediction[0] == 1:
-            parkinsons_diagnosis = "The person has Parkinson's disease"
-        else:
-            parkinsons_diagnosis = "The person does not have Parkinson's disease"
-
-    st.success(parkinsons_diagnosis)
+        Jitter_Abs = st.text_input('MDVP:Jitter(
